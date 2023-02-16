9b0c52fdc0c005c4f8e22222f2a83417f4376893
Artur <arturbr@gmail.com>
2023-02-16 17:07:36 -0300

    Separates app and server

--- src/app.js
+++ src/app.js
@@ -9,7 +9,4 @@ app.use(express.json()); //adds body (post) to req
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`listening on port ${port}...`);
-});
+module.exports = app;
