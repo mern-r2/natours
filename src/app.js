3377e85f311fbde411a1e03d451da152d650103e
Artur <arturbr@gmail.com>
2023-02-16 18:06:31 -0300

    Express serving static content

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
 app.use(express.json()); //adds body (post) to req
+app.use(express.static(`${__dirname}/../public`)); // serves static files in public folder
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
