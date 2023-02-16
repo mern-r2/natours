043a7a0129b2aa61e49fca702966d2fc95801586
Artur <arturbr@gmail.com>
2023-02-16 00:56:17 -0300

    Adds get, post and delete endpoints for a tour

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 
 const app = express();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const fileURI = `${__dirname}/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(fileURI));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -12,6 +13,57 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours
     }
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find(tour => tour.id === id);
+
+  if(id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
+app.post('/api/v1/tours/', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(fileURI, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+
+  if(id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
   });
 });
 
